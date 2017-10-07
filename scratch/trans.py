760ab9f6d2d6f1a287e0353fdca6ef67378f57f2
Nick Spinale <spinalen@carleton.edu>
2017-10-07 16:35:48 -0500

    Transform done

--- scratch/trans.py
+++ scratch/trans.py
@@ -6,6 +6,9 @@ from contextlib import contextmanager
 import itertools as it
 from numbers import Number
 
+__ALL__ = ['transform']
+
+
 def bigsubs(eqns, x, y):
     return [ eq.subs(x, y) for eq in eqns ]
 
@@ -21,7 +24,7 @@ def show(eqns):
         print(i, e)
     print()
 
-for subscript in [0, 1, 2, 3, 4]:
+for subscript in range(10):
     for var in 'abcdefghxyST':
         name = '{}{}'.format(var, subscript)
         globals()[name] = Symbol(name)
@@ -69,6 +72,7 @@ y3_ = T0 - g_.subs(x3, x3_)
 
 P_0 = 2*S0**2 + 2*g1*T0*S0 + 2*g0*T0**2 - T0**3 + h1*S0 + h0*T0
 
+# also:
 # T0_ = 2*T1
 # S0_ = 2*S1
 T0_ = T1/2
@@ -77,10 +81,35 @@ P_1 = P_0.subs(T0, T0_).subs(S0, S0_).expand().simplify()*8
 
 P_ = P_1.subs(g0, g0_).subs(g1, g1_).subs(h0, h0_).subs(h1, h1_).subs(f0, f0_).subs(f1, f1_).subs(f2, f2_).subs(f3, f3_)
 
-def go(c0_, c1_, c2_, c3_, c4_, alpha_):
-    return P_.subs(c0, c0_).subs(c1, c1_).subs(c2, c2_).subs(c3, c3_).subs(c4, c4_).subs(alpha, alpha_)
+foo = x0_.subs(x1, x1_).subs(x2, x2_).subs(x3, x3_).subs(T0, T0_).subs(S0, S0_)
+bar = y0_.subs(y1, y1_).subs(x2, x2_).subs(y2, y2_).subs(x3, x3_).subs(y3, y3_).subs(T0, T0_).subs(S0, S0_).subs(g0, g0_).subs(g1, g1_).subs(f0, f0_).subs(f1, f1_).subs(f2, f2_).subs(f3, f3_)
 
-print(go(21, 0, -14, 0, 2, 1).expand())
+weq = x**3 + a2*x**2 + a4*x + a6 - y**2 - a1*x*y - a3*y
+
+def to_w_coeffs(C):
+    a1 = -C.coeff(x*y)
+    a2 = C.coeff(x, n=2)
+    a3 = -C.subs(x, 0).coeff(y)
+    a4 = C.subs(y, 0).coeff(x)
+    a6 = C.subs(y, 0).subs(x, 0)
+    return a1, a2, a3, a4, a6
+
+def from_w_coeffs(a1_, a2_, a3_, a4_, a6_):
+    return weq.subs(a1, a1_).subs(a2, a2_).subs(a3, a3_).subs(a4, a4_).subs(a6, a6_)
+
+def transform(c0_, c1_, c2_, c3_, c4_, alpha_):
+    curve = -P_.subs(c0, c0_).subs(c1, c1_).subs(c2, c2_).subs(c3, c3_).subs(c4, c4_).subs(alpha, alpha_)
+    def coord_trans(x, y):
+        xt = foo.subs(c0, c0_).subs(c1, c1_).subs(c2, c2_).subs(c3, c3_).subs(c4, c4_).subs(alpha, alpha_).subs(T1, x).subs(S1, y)
+        yt = bar.subs(c0, c0_).subs(c1, c1_).subs(c2, c2_).subs(c3, c3_).subs(c4, c4_).subs(alpha, alpha_).subs(T1, x).subs(S1, y)
+        return xt, yt
+    def check_coords(x, y):
+        return p_0.subs(c0, c0_).subs(c1, c1_).subs(c2, c2_).subs(c3, c3_).subs(c4, c4_).subs(x0, x).subs(y0, y) == 0
+    return to_w_coeffs(curve.subs(T1, x).subs(S1, y)), coord_trans, check_coords
+
+def test():
+    test_curve, test_trans, check_coords = transform(21, 0, -14, 0, 2, 1)
+    assert check_coords(*test_trans(Rational(53,81), Rational(289,81)))
 
 # xx = x1_.subs()
 
