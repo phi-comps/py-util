a0a9f29b08153f80b65a87b1ea1a65e792668d00
Nick Spinale <spinalen@carleton.edu>
2017-11-03 13:42:26 -0500

    magic

--- a_zero.py
+++ a_zero.py
@@ -1,12 +1,12 @@
 
 # coding: utf-8
 
-# In[4]:
+# In[43]:
 
 
 import sympy as sy
 from sympy import Symbol, Eq, Rational, sqrt, solve
-from sympy.abc import a, b, c, d, m, x, y, alpha, beta, gamma
+from sympy.abc import a, b, c, d, m, x, y, alpha, beta, gamma, kappa
 
 from IPython.display import display, Latex
 
@@ -22,7 +22,7 @@ for subscript in range(5):
         name = '{}{}'.format(var, subscript)
         globals()[name] = Symbol(name)
 
-def displ(*x):
+def ds(*x):
     if __name__ == '__main__':
         display(*x)
 
@@ -34,13 +34,50 @@ def show(eqs):
     )))
 
 
-# In[9]:
+# In[44]:
 
 
-displ(aa_)
+ds(aa_)
 l, r = solve(Eq(0, aa_), m)
 l, r
 
+
+# In[45]:
+
+
+kk_ = -2*d**4 + 8*d**2 + 4
+m_ = r.subs(sqrt(kk_), kappa)
+bb_.subs(m, m_)
+
+
+# In[66]:
+
+
+ds(Eq(gamma, aa_*beta + bb_))
+ds(Eq(0, aa_))
+ds(Eq(gamma, bb_))
+aa__ = (-aa_/d**2).expand()
+ds((Eq(d, 0), Eq(0, aa__)))
+ds(Eq(-gamma-m, -bb_-m))
+ds(((-bb_-m)-4*m*aa_).expand())
+ds((((-bb_-m)-4*m*aa_).expand()/d**4).expand())
+ds((((-bb_-m)-4*m*aa_).expand()/d**4).expand() + 7*aa__/2)
+ds(((((-bb_-m)-4*m*aa_).expand()/d**4).expand() + 7*aa__/2).factor())
+
+
+# In[35]:
+
+
+(-((-bb_-m-4*aa_*m).expand()*-64/(d**4) + 14*((4*aa_/d**2).expand()*4).expand()).expand()).factor()
+
+
+# In[28]:
+
+
+((4*aa_/d**2).expand()*4).expand()
+
+
+# ## Curve for $\kappa$
 
 # In[3]:
 
